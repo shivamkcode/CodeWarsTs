a072ddc4ac25febb52183762a698b50f5a3803af
shivamKcode <shivambaniya246@gmail.com>
2024-02-26 14:31:26 -0500

    Added EvenorOdd

---
+++ EvenorOdd.ts
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+export function evenOrOdd(n: number): string {
+  return n % 2 === 0 ? "Even" : "Odd";
+}
+
+// export const evenOrOdd = (n: number): string => (n % 2 === 0 ? "Even" : "Odd");
