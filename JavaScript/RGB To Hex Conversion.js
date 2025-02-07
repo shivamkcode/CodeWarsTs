7354aacd9b8506ca4a865ceb25844f12e49f38bd
shivamKcode <shivambaniya246@gmail.com>
2025-02-06 20:58:47 -0500

    solution for the hashtag generator Kata

--- JavaScript/RGB To Hex Conversion.js
+++ JavaScript/RGB To Hex Conversion.js
@@ -10,31 +10,46 @@
 // 148, 0, 211   --> "9400D3"
 
 function rgb(r, g, b) {
-    const div = (x) => {
-      if (x < 0) {
-        return `00`;
-      }
-      if (x > 255) {
-        return "FF";
-      }
-      let num = String((x / 16).toFixed(2)).split(".");
-      let num1 = Number(num[0]);
-      let num2 = Math.round((num[1] / 100) * 16);
-      let code = y => y <= 9
-            ? y
-            : y === 10
-            ? "A"
-            : y === 11
-            ? "B"
-            : y === 12
-            ? "C"
-            : y === 13
-            ? "D"
-            : y === 14
-            ? "E"
-            : "F";
-      return `${code(num1)}${code(num2)}`;
-    };
-  
-    return div(r) + div(g) + div(b);
+  const div = (x) => {
+    if (x < 0) {
+      return `00`;
+    }
+    if (x > 255) {
+      return "FF";
+    }
+    let num = String((x / 16).toFixed(2)).split(".");
+    let num1 = Number(num[0]);
+    let num2 = Math.round((num[1] / 100) * 16);
+    let code = (y) =>
+      y <= 9
+        ? y
+        : y === 10
+        ? "A"
+        : y === 11
+        ? "B"
+        : y === 12
+        ? "C"
+        : y === 13
+        ? "D"
+        : y === 14
+        ? "E"
+        : "F";
+    return `${code(num1)}${code(num2)}`;
+  };
+
+  return div(r) + div(g) + div(b);
+}
+
+function rgb(r, g, b) {
+  return toHex(r) + toHex(g) + toHex(b);
+}
+
+function toHex(d) {
+  if (d < 0) {
+    return "00";
   }
+  if (d > 255) {
+    return "FF";
+  }
+  return ("0" + Number(d).toString(16)).slice(-2).toUpperCase();
+}
