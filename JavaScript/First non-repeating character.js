f951723b7e30f4b2ada6d0a03c58f1515047c5da
shivamKcode <shivambaniya246@gmail.com>
2025-02-09 18:34:17 -0500

    completed Product of consecutive Fib numbers kata

--- JavaScript/First non-repeating character.js
+++ JavaScript/First non-repeating character.js
@@ -21,3 +21,10 @@ function firstNonRepeatingLetter(s) {
     .join("")
     .charAt(0);
 }
+
+function firstNonRepeatingLetter(s) {
+  var t = s.toLowerCase();
+  for (var x = 0; x < t.length; x++)
+    if (t.indexOf(t[x]) === t.lastIndexOf(t[x])) return s[x];
+  return "";
+}
