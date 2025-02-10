c6a27c32ee206792fdf80078dab726cd7a0ad321
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2025-02-10 15:41:05 -0500

    Save Sum of a sequence.js.

--- JavaScript/Sum of a sequence.js
+++ JavaScript/Sum of a sequence.js
@@ -22,3 +22,11 @@ const sequenceSum = (begin, end, step) => {
     }
     return total
 };
+
+const sequenceSum = (begin, end, step) => {
+    var sum = 0;
+    for (var i = begin; i <= end; i += step) {
+        sum += i;
+    }
+    return sum;
+};
