a087b41b47fda3710f20b77865df9a86a2eb6b06
Shivam Kumar <108574110+shivamkcode@users.noreply.github.com>
2025-02-14 13:25:53 -0500

    Save Get Planet Name By ID.js.

--- JavaScript/Get Planet Name By ID.js
+++ JavaScript/Get Planet Name By ID.js
@@ -10,42 +10,40 @@ function getPlanetName(id) {
     switch (id) {
         case 1:
             name = 'Mercury'
-            breakfunction getPlanetName(id) {
-                var name;
-                console.log(id)
-                switch (id) {
-                case 1:
-                    name = 'Mercury'
-                    break
-                case 2:
-                    name = 'Venus'
-                    break
-                case 3:
-                    name = 'Earth'
-                    break
-                case 4:
-                    name = 'Mars'
-                    break
-                case 2:
-                    name = 'Venus'
-                    break
-                case 3:
-                    name = 'Earth'
-                    break
-                case 4:
-                    name = 'Mars'
-                    break
-                case 5:
-                    name = 'Jupiter'
-                    break
-                case 6:
-                    name = 'Saturn'
-                    break
-                case 7:
-                    name = 'Uranus'
-                    break
-                case 8:
-                    name = 'Neptune'
-                }
+            break
+        case 2:
+            name = 'Venus'
+            break
+        case 3:
+            name = 'Earth'
+            break
+        case 4:
+            name = 'Mars'
+            break
+        case 2:
+            name = 'Venus'
+            break
+        case 3:
+            name = 'Earth'
+            break
+        case 4:
+            name = 'Mars'
+            break
+        case 5:
+            name = 'Jupiter'
+            break
+        case 6:
+            name = 'Saturn'
+            break
+        case 7:
+            name = 'Uranus'
+            break
+        case 8:
+            name = 'Neptune'
+    }
 
-                return name;
+    return name;
+}
+
+const BODIES = ['Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+const getPlanetName = id => BODIES[id];
